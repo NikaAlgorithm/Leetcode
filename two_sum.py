f2cceb4ce43cfc822b776e34d878dea6632ae59d
Nika <70407540+NikaAlgorithm@users.noreply.github.com>
2025-08-19 11:51:06 +0330

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,7 @@
+"""
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+"""
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
